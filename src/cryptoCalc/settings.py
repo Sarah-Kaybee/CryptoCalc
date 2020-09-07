a7581818cd35ffdbff19c2dd29042dc33504a6cb
Sarah Kay <51705696+Sarah-Kaybee@users.noreply.github.com>
2020-09-07 20:34:52 +1000

    Fixed main.css and added static

--- src/cryptoCalc/settings.py
+++ src/cryptoCalc/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print("BASE_DIR = ", BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/calculator')
+]
